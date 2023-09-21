b13f0281a9753ad3bb9cb44b1717cea77da183bf
Javier Valdes <jesusjvaldess7@outlook.com>
2023-09-21 18:48:57 +0200

    Delete animation-reveal

--- assets/js/reveal.js
+++ assets/js/reveal.js
@@ -1,25 +1,25 @@
-window.sr = ScrollReveal();
+// window.sr = ScrollReveal();
 
-sr.reveal(".curriculum__title", {
-  duration: 2000,
-  origin: "top",
-  distance: "1rem",
-});
+// sr.reveal(".curriculum__title", {
+//   duration: 2000,
+//   origin: "top",
+//   distance: "1rem",
+// });
 
-sr.reveal(".curriculum__left", {
-  duration: 2000,
-  origin: "left",
-  distance: "7rem",
-});
+// sr.reveal(".curriculum__left", {
+//   duration: 2000,
+//   origin: "left",
+//   distance: "7rem",
+// });
 
-sr.reveal(".curriculum__right", {
-  duration: 2000,
-  origin: "right",
-  distance: "7rem",
-});
+// sr.reveal(".curriculum__right", {
+//   duration: 2000,
+//   origin: "right",
+//   distance: "7rem",
+// });
 
-sr.reveal(".certificates__title", {
-  duration: 2000,
-  origin: "top",
-  distance: "7rem",
-});
+// sr.reveal(".certificates__title", {
+//   duration: 2000,
+//   origin: "top",
+//   distance: "7rem",
+// });
