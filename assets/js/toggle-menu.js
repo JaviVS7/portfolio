78dc3f86128af90e404b39029de08db2e62bf671
Javier Valdes <jesusjvaldess7@outlook.com>
2023-09-28 05:18:47 +0200

    Add icon darktheme responsive

--- assets/js/toggle-menu.js
+++ assets/js/toggle-menu.js
@@ -9,12 +9,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     if (!visible) {
       aside.classList.add("layout__aside--visible");
-      button.style.background = "var(--second-color)";
+      // button.style.background = "var(--second-color)";
       icon_bars.style.display = "none";
       icon_xmark.style.display = "inline-block";
     } else {
       aside.classList.remove("layout__aside--visible");
-      button.style.background = "var(--main-color)";
+      // button.style.background = "var(--main-color)";
       icon_bars.style.display = "inline-block";
       icon_xmark.style.display = "none";
     }
